eb4721d8acf5b32dcd79c9b0eeb88f044b05399c
Mr.Vinicius <viniciusrebelo487@gmail.com>
2020-06-17 20:01:35 -0300

    Moviments of  snake

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 let canvas = document.getElementById("snake");
-let context = canvas.getContext("2d");
+let context = canvas.getContext('2d');
 let = box = 32;
-
+let snake = [];
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
+}
+let direction = 'right';
 function criarBG() {
-  context.fillStyle = "lightgreen";
+  context.fillStyle = 'lightgreen';
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
-criarBG();
+
+
+function createSnake() {
+  for (let i = 0; i < snake.length; i++) {
+    context.fillStyle = 'green';
+    context.fillRect(snake[i].x, snake[i].y, box, box)
+  }
+}
+
+function startGame() {
+  criarBG();
+  createSnake();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (direction == 'right') snakeX += box;
+  if (direction == 'left') snakeX -= box;
+  if (direction == 'up') snakeY -= box;
+  if (direction == 'down') snakeY += box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+  snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
